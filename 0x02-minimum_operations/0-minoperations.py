e0dd6e1ea92712530327dd0e98fb5b684652633c
NathyG2524 <nathygirma2524@gmail.com>
2022-06-30 12:19:54 +0300

    minimum operations to copy and paste a single string n times

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -73,11 +73,11 @@ def minOperations(n):
         return n
     if (isfdivisible_by2(n)):
         count = count_no_iteration(n, 2)
-        return count * 2
+        return count 
 
     if isdivisible_by3(n) and isdivisible_by2(n):
         count = count_no_iteration(n, 2)
-        return (count * 2 + 3)
+        return (count )
 
     if isdivisible_by3(n) and not isdivisible_by2(n):
         return int(n / 3 + 3)
@@ -86,4 +86,4 @@ def minOperations(n):
         return count_no_iteration(n, 2)
 
 
-print(minOperations(9))
+# print(minOperations(12))
