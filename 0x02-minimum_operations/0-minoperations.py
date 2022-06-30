3a32dab8d659e01ca61c61b6686ccab1d886efd4
NathyG2524 <nathygirma2524@gmail.com>
2022-06-30 22:43:19 +0300

    change the algorithms

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -20,6 +20,8 @@ def isprime(n):
 def minOperations(n):
     """minimum operations"""
     listFac = []
+    if n < 1:
+        return 0
     if n == 1:
         return 1
     for i in range(2, int(n)+1):
