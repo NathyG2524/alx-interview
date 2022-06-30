23db34dd0d7cbcdd6268724f8d4aa265dea42dc3
NathyG2524 <nathygirma2524@gmail.com>
2022-06-30 22:27:47 +0300

    change the algorithms

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -17,72 +17,17 @@ def isprime(n):
         return True
 
 
-def isfdivisible_by2(n):
-    """check for fully divisible by 2"""
-    if n == 2:
-        return True
-    if n % 2 == 0:
-        return isfdivisible_by2(n / 2)
-    else:
-        return False
-
-
-def isdivisible_by2(n):
-    """check divisible by 2"""
-    if n < 2:
-        return False
-    if n % 2 == 0:
-        return True
-    else:
-        return False
-
-
-def isdivisible_by3(n):
-    """check divisible by 3"""
-    if n < 3:
-        return False
-    if n % 3 == 0:
-        return True
-    else:
-        return False
-
-
-def count_no_iteration(n, d):
-    """check for iteration"""
-    count = 0
-    i = n
-    while(i % d == 0):
-
-        count = count + 1
-        i = i / d
-
-        if isprime(int(i)):
-            count = (2 * count) + int(i)
-            break
-
-    return count
-
-
 def minOperations(n):
-    """count minmum operation"""
-    if n < 1:
-        return 0
-    if n == 1:
-        return 1
-    if (isprime(n)):
-        return n
-    if (isfdivisible_by2(n)):
-        count = count_no_iteration(n, 2)
-        return int(count)
+    """minimum operations"""
+    listFac = []
+    for i in range(2, int(n)+1):
+        while (n % i == 0):
+            if (isprime(i)):
+                listFac.append(i)
+            n = n / i
 
-    if isdivisible_by3(n) and isdivisible_by2(n):
-        count = count_no_iteration(n, 2)
-        return int(count)
+    lisum = 0
+    for i in listFac:
+        lisum = lisum + i
 
-    if isdivisible_by3(n) and not isdivisible_by2(n):
-        return int(n / 3 + 3)
-
-    if (n % 2 == 0):
-        return int(count_no_iteration(n, 2))
-
-
+    return lisum
