20c2611ef9de7db92e4cd2b10ba7059abf10475b
NathyG2524 <nathygirma2524@gmail.com>
2022-07-06 14:37:57 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,14 +7,14 @@ import fileinput
 
 if __name__ == "__main__":
     file_list = []
-    status_list =[]
+    status_list = []
     counter = 0
     total_size = 0
 
     try:
         for line in fileinput.input():
-        # counter += 1
-        
+            # counter += 1
+
             line_list = line.split()
             ip = line_list[0]
             status_code = line_list[7]
@@ -22,8 +22,7 @@ if __name__ == "__main__":
             file_list.append(file_size)
             status_list.append(status_code)
             # print(counter)
-        
-            
+
             for size in file_list:
                 total_size = total_size + int(size)
             if (counter % 10 == 0):
@@ -37,13 +36,11 @@ if __name__ == "__main__":
                 number = status_list.count(code)
                 if (counter % 10 == 0):
                     print(f'{code}: {number}')
-            
-            counter += 1
-            
-    except KeyboardInterrupt:
-            
 
-        
+            counter += 1
+
+    except KeyboardInterrupt:
+
         for size in file_list:
             total_size = total_size + int(size)
         print(f'File size: {total_size}')
@@ -53,14 +50,11 @@ if __name__ == "__main__":
         codetuple.sort()
         for code in codetuple:
             number = status_list.count(code)
-            
+
             print(f'{code}: {number}')
         print(error)
-            
+
 # ip_address = f'{sys.argv[0]}.{sys.argv[1]}.{sys.argv[2]}.{sys.argv[3]}'
 # date = sys.argv[0]
 # status_code = sys.argv[0]
 # file_size = sys.argv[0]
-
-
-
