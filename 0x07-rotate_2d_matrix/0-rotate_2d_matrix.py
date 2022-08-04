5d92b47cd9de40e547d5d74105a64f7542241211
NathyG2524 <nathygirma2524@gmail.com>
2022-08-04 22:15:12 +0300

    [fix]: remove return

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -8,35 +8,11 @@ You can assume the matrix will have 2 dimensions and will not be empty.
 
 
 def rotate_2d_matrix(matrix):
-    """rotate nxn matrix"""
-    newMatrix = []
-    newArray = []
-    lastMatrix = []
-    # for i in matrix:
-    #     newMatrix.append(newArray)
     for i in range(len(matrix)):
-        newArray = []
-        for j in range(len(matrix[i])):
-            newNum = matrix[i][j]
-            newArray.append(newNum)
-        newMatrix.append(newArray)
-
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            newMatrix[j][i] = matrix[i][j]
-
-    for i in range(len(newMatrix)):
-        newArray = []
-        for j in range(len(newMatrix[i])):
-            newNum = newMatrix[i][j]
-            newArray.append(newNum)
-        lastMatrix.append(newArray)
-
-    for i in range(len(newMatrix)):
-        k = 0
-        for j in range(len(newMatrix[i])):
-            n = len(matrix[i]) - 1
-            lastMatrix[i][n - k] = newMatrix[i][j]
-            k += 1
+        for j in range(i):
+            a, b = matrix[i][j], matrix[j][i]
+            matrix[i][j], matrix[j][i] = b, a
+    for row in matrix:
+        row.reverse()
         # print(n)
     # return (lastMatrix)
