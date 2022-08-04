884e07205dd262b5aee4f518b563bd193af100e8
NathyG2524 <nathygirma2524@gmail.com>
2022-08-04 20:22:33 +0300

    [update]: documentation

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
+"""
+Prototype: def rotate_2d_matrix(matrix):
+Do not return anything. The matrix must be edited in-place.
+You can assume the matrix will have 2 dimensions and will not be empty.
+"""
 def rotate_2d_matrix(matrix):
+    """rotate nxn matrix"""
     newMatrix = []
     newArray = []
     lastMatrix = []
